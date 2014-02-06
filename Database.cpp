2159627a98339e35bad7cb75816812b7e8b13a76
Mohammad Anees <aman6129@neo.tamu.edu>
2014-02-06 14:51:09 -0600

    Commit 2 Initial source code ~~~~~~~~~~~~~~~~~~~

---
+++ Database.cpp
@@ -0,0 +1,62 @@
+#include "Database.h"
+#include <fstream>
+
+
+void Database::create(string table_name, vector<string> attributes, vector<string> attribute_types, vector<string> keys)
+{
+	// push back empty table to relation list and get reference to table
+	RELATION_LIST.push_back(vector<vector<string>>());
+	vector<vector<string>> & relation_table = RELATION_LIST[RELATION_LIST.size() - 1];
+
+	// push back empty row and intialize with title and keys
+	relation_table.push_back(vector<string>());
+	relation_table[0].push_back(table_name);
+	relation_table[0].insert(relation_table[0].end(), keys.begin(), keys.end());
+
+	// add attribute names and attribute types
+	relation_table.push_back(attributes);
+	relation_table.push_back(attribute_types);
+
+}
+
+
+int Database::get_relation_index(string table_name)
+{
+	int INDEX = -1;
+
+	for (unsigned int i = 0; i<RELATION_LIST.size(); i++)
+	if (RELATION_LIST[i][0][0] == table_name)
+		INDEX = i;
+	return INDEX;
+}
+
+int Database::get_view_index(string table_name)
+{
+	int INDEX = -1;
+
+	for (unsigned int i = 0; i<VIEW_LIST.size(); i++)
+	if (VIEW_LIST[i][0][0] == table_name)
+		INDEX = i;
+	return INDEX;
+}
+
+int Database::get_attribute_index(TableType type, int table_index, string attribute_name)
+{
+	int COLUMN_INDEX = -1;
+	if (type == RELATION)
+	{
+		for (unsigned int i = 0; i<RELATION_LIST[table_index][1].size(); i++)
+		if (attribute_name == RELATION_LIST[table_index][1][i])
+			COLUMN_INDEX = i;
+	}
+	else
+	{
+		for (unsigned int i = 0; i<VIEW_LIST[table_index][1].size(); i++)
+		if (attribute_name == VIEW_LIST[table_index][1][i])
+			COLUMN_INDEX = i;
+	}
+	return COLUMN_INDEX;
+}
+
+
+
---
+++ Database.h
@@ -0,0 +1,36 @@
+#ifndef DATABASE_H_
+#define DATABASE_H_
+
+#include <cstdarg>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Database{
+private:
+	vector<vector<vector<string>>> RELATION_LIST;
+	vector<vector<vector<string>>> VIEW_LIST;
+	enum TableType {VIEW, RELATION};
+
+public:
+	void select(string view_name, string in_table_name, int row_index);
+	void project(string view_name, string in_table_name, vector<string> attributes);
+	void rename(string view_name, string in_table_name, vector<string> attributes);
+	void set_union(string view_name, string table1_name, string table2_name);
+	void set_difference(string view_name, string table1_name, string table2_name);
+	void cross_product(string view_name, string table1_name, string table2_name);
+	void natural_join(string view_name, string table1_name, string table2_name);
+
+	void create(string table_name, vector<string> attributes, vector<string> attribute_types, vector<string> keys);
+	void update(string relation_name, vector<string> attribute, vector<string> data, int row_index);
+	void insert_tuple(string relation_name, vector<string> tuple);
+	void insert_view(string relation_name, string view_name);
+	void remove(string table_name, int row_index);
+
+	int get_relation_index(string table_name);
+	int get_view_index(string table_name);
+	int get_attribute_index(TableType type, int table_index, string attribute_name);
+};
+
+#endif
